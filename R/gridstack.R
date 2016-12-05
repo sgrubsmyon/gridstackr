58a62ec6ceab8d8ed08abb16855d28a258d2655c
Joshua <jbkunst@gmail.com>
2016-12-05 18:31:35 -0300

    adding conf opts

--- R/gridstack.R
+++ R/gridstack.R
@@ -1,22 +1,33 @@
+#' @export
+initGS <- function(cell_height = 80, vertical_margin = 10, handles = NULL) {
+
+  shiny::tags$head()
+
+}
+
+
+
 #' @importFrom htmltools tags
 #' @export
 div <- htmltools::tags$div
 
+#' Gridstack container
 #' @export
 gridstack <- function(...) {
 
   div(class = "grid-stack", ...)
 
 }
-}
 
+#' Gridstack Item
 #' @export
 gs_item <- function(..., x = 0, y = 0, w = 4, h = 4) {
 
-  div(class = "grid-stack-item",
-      data-gs-x = x, data-gs-y = y,
-      data-gs-w = w, data-gs-h = h,
-      div(class = "grid-stack-item-content", ...)
-      )
+  div(
+    class = "grid-stack-item",
+    "data-gs-x" = x, "data-gs-y" = y,
+    "data-gs-w" = w, "data-gs-h" = h,
+    div(class = "grid-stack-item-content", ...)
+    )
 
 }
