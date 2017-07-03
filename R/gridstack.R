f636f9d5dc02dbb1dd079959771ca66f8986bed1
mdlama <mdlama@wm.edu>
2017-07-03 10:49:36 -0400

    Fixed issue.  gridstack.css wasn't loading properly.

--- R/gridstack.R
+++ R/gridstack.R
@@ -7,25 +7,42 @@ initGS <- function() {
   addResourcePath("lodash", system.file("lodash", package = "gridstackr", mustWork = TRUE))
   addResourcePath("jquery-ui", system.file("jquery-ui", package = "gridstackr", mustWork = TRUE))
 
-  css <- ".grid-stack {background: lightgoldenrodyellow;} .grid-stack-item-content {color: #2c3e50; background-color: #18bc9c;}"
-  initconf <- "$(function () { $('.grid-stack').gridstack({ float:false, width: 12, cellHeight: 80, verticalMargin: 10}); });"
+  css <- "
+.grid-stack {
+  background: lightgoldenrodyellow;
+}
+
+.grid-stack-item-content {
+  color: #2c3e50;
+  text-align: center;
+  background-color: #18bc9c;
+}"
+
+  initconf <- "
+$(function () {
+  var options = {
+    float: true,
+    cellHeight: 80,
+    verticalMargin: 10
+  };
+  $('.grid-stack').gridstack(options);
+});";
 
   singleton(
     tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = file.path("gridstackjs", "gridstack.css")),
+
       tags$script(src = file.path("jquery-ui", "jquery-ui.js")),
       tags$script(src = file.path("lodash", "lodash.min.js")),
 
-      # tags$script(src = file.path("gridstackjs", "gridstack.all.js")),
       tags$script(src = file.path("gridstackjs", "gridstack.min.js")),
       tags$script(src = file.path("gridstackjs", "gridstack.jQueryUI.js")),
-
-      tags$link(src = file.path("gridstackjs", "gridstack.css")),
 
       tags$style(HTML(css)),
       tags$script(HTML(initconf))
 
-      )
     )
+  )
 }
 
 #' Gridstack container
@@ -51,6 +68,6 @@ gs_item <- function(..., x = 0, y = 0, w = 4, h = 4) {
     "data-gs-x" = x, "data-gs-y" = y,
     "data-gs-width" = w, "data-gs-height" = h,
     tags$div(class = "grid-stack-item-content", ...)
-    )
+  )
 
 }
