64c1c57eddc267d8c44fab61b1ddd625b8dc282b
mdlama <mdlama@wm.edu>
2017-07-10 13:32:48 -0400

    Initial code for HTMLWidget template.

---
+++ R/gridstackr.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+gridstackr <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'gridstackr',
+    x,
+    width = width,
+    height = height,
+    package = 'gridstackr',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for gridstackr
+#'
+#' Output and render functions for using gridstackr within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a gridstackr
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name gridstackr-shiny
+#'
+#' @export
+gridstackrOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'gridstackr', width, height, package = 'gridstackr')
+}
+
+#' @rdname gridstackr-shiny
+#' @export
+renderGridstackr <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, gridstackrOutput, env, quoted = TRUE)
+}
